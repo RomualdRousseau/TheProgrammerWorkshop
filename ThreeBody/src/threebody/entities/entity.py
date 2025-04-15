0d7158c4a15ceea4ce167d6c7656a15a56427cca
Romuald Rousseau <romuald.rousseau@servier.com>
2025-04-15 10:03:59 +0200

    efeat: Add initial angular velocity

--- ThreeBody/src/threebody/entities/entity.py
+++ ThreeBody/src/threebody/entities/entity.py
@@ -6,4 +6,5 @@ import pyray as pr
 class Entity(Protocol):
     mass: float
     force: pr.Vector3
+    rho: pr.Vector3
     pos: pr.Vector3
--- ThreeBody/src/threebody/utils/physic.py
+++ ThreeBody/src/threebody/utils/physic.py
@@ -6,6 +6,12 @@ G = 6.67e-8  # km3 Gt−1 s−2
 SOFT = 1e7
 
 
+def centrifuge_force_on_axis_y(e: Entity, w: float) -> pr.Vector3:
+    u = pr.Vector3(0, w, 0)
+    v = pr.vector3_normalize(pr.Vector3(e.pos.x, 0, e.pos.z))
+    return pr.vector3_scale(pr.vector3_cross_product(u, v), e.mass)
+
+
 def gravity_center(entities: list[Entity]) -> pr.Vector3:
     sum = pr.vector3_zero()
     for entity in entities:
