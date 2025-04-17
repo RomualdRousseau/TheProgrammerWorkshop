6678adacbd888279d86554e869c2c9083364e00a
Romuald Rousseau <romuald.rousseau@servier.com>
2025-04-17 11:30:15 +0200

    chore: Cleanup

--- ThreeBody/src/threebody/entities/star.py
+++ ThreeBody/src/threebody/entities/star.py
@@ -21,17 +21,17 @@ class Star:
         self.sphere = Sphere(TRAIL_SIZE)
 
     def update(self, dt: float) -> None:
-        self.update_physic(dt)
+        self.rho = euler_integrate(self.rho, self.force, dt)
+        self.pos = euler_integrate(self.pos, pr.vector3_scale(self.rho, 1.0 / self.mass), dt)
+        self.reset_force()
 
     def draw(self) -> None:
         scaled_pos = pr.vector3_scale(self.pos, STAR_DISTANCE_SCALE)
         scaled_radius = self.mass * STAR_RADIUS_SCALE
         self.sphere.draw(scaled_pos, scaled_radius, pr.RAYWHITE)
 
+    def reset_force(self):
+        self.force = pr.vector3_zero()
+
     def apply_force(self, f: pr.Vector3):
         self.force = pr.vector3_add(self.force, f)
-
-    def update_physic(self, dt: float) -> None:
-        self.rho = euler_integrate(self.rho, self.force, dt)
-        self.pos = euler_integrate(self.pos, pr.vector3_scale(self.rho, 1.0 / self.mass), dt)
-        self.force = pr.vector3_zero()
