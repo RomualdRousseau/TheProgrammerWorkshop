9f493a35b5d6b436564e923378e7b971518189f6
Romuald Rousseau <romualdrousseau@gmail.com>
2025-04-20 09:42:53 +0200

    feat: Add number of starts as cli arguments

--- ThreeBody/src/threebody/scenes/galaxy.py
+++ ThreeBody/src/threebody/scenes/galaxy.py
@@ -1,3 +1,5 @@
+import sys
+
 import pyray as pr
 
 from threebody.entities.star import STAR_DISTANCE_SCALE, Star
@@ -6,7 +8,7 @@ from threebody.utils.vector import vector3_random
 
 # Simulation of N body system with a total mass M equivalent to the sun mass within a diameter D of 1 AU, with a
 # timestep T of 10 days.
-N = 20  # stars
+N = 3  # stars
 M = 1.9e18  # Gt
 D = 1.5e8  # km
 T = 8.64e5  # s
@@ -22,7 +24,11 @@ class Context:
 
 
 def init():
-    Context.stars = [Star(pr.vector3_scale(vector3_random(), D / 2.0), M / N) for _ in range(N)]
+    num_stars = N
+    if len(sys.argv) > 1:
+        num_stars = int(sys.argv[1])
+
+    Context.stars = [Star(pr.vector3_scale(vector3_random(), D / 2.0), M / num_stars) for _ in range(num_stars)]
     Context.camera = pr.Camera3D(CAMERA_EYE, CAMERA_TARGET, CAMERA_UP, 45, pr.CameraProjection.CAMERA_PERSPECTIVE)
 
 
@@ -34,8 +40,9 @@ def update(dt: float):
     Context.camera.target = pr.vector3_scale(gravity_center(Context.stars), STAR_DISTANCE_SCALE)  # type: ignore
     pr.update_camera(Context.camera, pr.CameraMode.CAMERA_THIRD_PERSON)
 
-    for i in range(N):
-        for j in range(i + 1, N):
+    num_stars = len(Context.stars)
+    for i in range(num_stars):
+        for j in range(i + 1, num_stars):
             f_i, f_j = gravity_force(Context.stars[i], Context.stars[j])
             Context.stars[i].apply_force(f_i)
             Context.stars[j].apply_force(f_j)
