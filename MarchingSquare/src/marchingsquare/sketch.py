f9babdf4b075d06bab1900d4401728c3db2bff2b
Romuald Rousseau <romualdrousseau@gmail.com>
2025-04-21 15:19:34 +0200

    feat: Gnerate metaballs

--- MarchingSquare/src/marchingsquare/sketch.py
+++ MarchingSquare/src/marchingsquare/sketch.py
@@ -1,18 +1,43 @@
 import pyray as pr
 
+from marchingsquare import WINDOW_HEIGHT, WINDOW_WIDTH
+from marchingsquare.metaballs import (
+    Metaball,
+    create_metaballs,
+    generate_metaballs,
+    update_metaballs,
+)
+
+N = 5
+
+
+class Context:
+    metaballs: list[Metaball]
+    texture: pr.Texture
+
 
 def init() -> None:
-    pass
+    Context.metaballs = create_metaballs(N)
+    image = generate_metaballs(Context.metaballs)
+    Context.texture = pr.load_texture_from_image(image)
 
 
 def release() -> None:
-    pass
+    pr.unload_texture(Context.texture)
 
 
 def update(dt: float) -> None:
-    pass
+    Context.metaballs = update_metaballs(Context.metaballs, dt)
 
 
 def draw() -> None:
-    pr.clear_background(pr.BLACK)
-    pr.draw_text("Hello the world", 10, 10, 20, pr.RAYWHITE)
+    image = generate_metaballs(Context.metaballs)
+    pr.update_texture(Context.texture, image.data)
+    pr.draw_texture_pro(
+        Context.texture,
+        (0, 0, Context.texture.width, Context.texture.height),
+        (0, 0, WINDOW_WIDTH, WINDOW_HEIGHT),
+        (0, 0),
+        0,
+        pr.WHITE,
+    )
