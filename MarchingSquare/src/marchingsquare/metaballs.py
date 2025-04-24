6f0db578d662d4905b5db8c5d76e3ac165d015d5
Romuald Rousseau <romualdrousseau@gmail.com>
2025-04-24 10:39:28 +0200

    feat: Add palettes (fire, hsv)

--- MarchingSquare/src/marchingsquare/metaballs.py
+++ MarchingSquare/src/marchingsquare/metaballs.py
@@ -27,8 +27,8 @@ def update_metaballs(metaballs: list[Metaball], dt: float) -> list[Metaball]:
     return [update_metaball_physic(metaball, dt) for metaball in metaballs]
 
 
-def generate_metaballs(metaballs: list[Metaball]) -> pr.Image:
-    pixels = generate_metaballs_pixels(metaballs)
+def generate_metaballs(metaballs: list[Metaball], palette: np.ndarray) -> pr.Image:
+    pixels = generate_metaballs_pixels(metaballs, palette)
     return pr.Image(pixels, FIELD_WIDTH, FIELD_HEIGHT, 1, pr.PixelFormat.PIXELFORMAT_UNCOMPRESSED_R8G8B8A8)
 
 
@@ -45,13 +45,13 @@ def update_metaball_physic(metabball: Metaball, dt: float) -> Metaball:
 
 
 @njit
-def generate_metaballs_pixels(metaballs: list[Metaball]) -> np.ndarray:
+def generate_metaballs_pixels(metaballs: list[Metaball], palette: np.ndarray) -> np.ndarray:
     pixels = np.zeros((FIELD_HEIGHT, FIELD_WIDTH, 4), dtype=np.uint8)
     for y in range(FIELD_HEIGHT):
         for x in range(FIELD_WIDTH):
             hue = sum([function_metaball(metaball, x, y) for metaball in metaballs])
-            hue = min(max(360 * hue, 0), 359)
-            pixels[y, x] = hsv_to_rgb(hue, 100.0, 100.0)
+            hue = int(min(max(255 * hue, 0), 255))
+            pixels[y, x] = palette[hue]
     return pixels
 
 
@@ -60,40 +60,3 @@ def function_metaball(metaball: Metaball, x: float, y: float) -> float:
     e_x, e_y, e_r, _, _ = metaball
     dist = (e_x - x) ** 2 + (e_y - y) ** 2
     return 1.0 if dist == 0.0 else e_r**2 / dist
-
-
-@njit
-def hsv_to_rgb(h: float, s: float, v: float) -> list[int]:
-    # Normalize the HSV values
-    h = h / 360.0
-    s = s / 100.0
-    v = v / 100.0
-
-    # Calculate the intermediate values
-    c = v * s
-    x = c * (1 - abs((h * 6) % 2 - 1))
-    m = v - c
-
-    # Determine the RGB values based on the Hue (H)
-    if 0 <= h < 1 / 6:
-        r_prime, g_prime, b_prime = c, x, 0
-    elif 1 / 6 <= h < 2 / 6:
-        r_prime, g_prime, b_prime = x, c, 0
-    elif 2 / 6 <= h < 3 / 6:
-        r_prime, g_prime, b_prime = 0, c, x
-    elif 3 / 6 <= h < 4 / 6:
-        r_prime, g_prime, b_prime = 0, x, c
-    elif 4 / 6 <= h < 5 / 6:
-        r_prime, g_prime, b_prime = x, 0, c
-    elif 5 / 6 <= h < 1:
-        r_prime, g_prime, b_prime = c, 0, x
-    else:
-        r_prime, g_prime, b_prime = 0, 0, 0
-
-    # Convert the intermediate RGB values to the final RGB values
-    r = (r_prime + m) * 255
-    g = (g_prime + m) * 255
-    b = (b_prime + m) * 255
-
-    # Return the RGB values as integers
-    return [int(r), int(g), int(b), 255]
--- MarchingSquare/src/marchingsquare/sketch.py
+++ MarchingSquare/src/marchingsquare/sketch.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pyray as pr
 
 from marchingsquare import WINDOW_HEIGHT, WINDOW_WIDTH
@@ -7,18 +8,21 @@ from marchingsquare.metaballs import (
     generate_metaballs,
     update_metaballs,
 )
+from marchingsquare.palettes import generate_fire_palette
 
 N = 5
 
 
 class Context:
     metaballs: list[Metaball]
+    palette: np.ndarray
     texture: pr.Texture
 
 
 def init() -> None:
     Context.metaballs = create_metaballs(N)
-    image = generate_metaballs(Context.metaballs)
+    Context.palette = generate_fire_palette()
+    image = generate_metaballs(Context.metaballs, Context.palette)
     Context.texture = pr.load_texture_from_image(image)
 
 
@@ -31,8 +35,9 @@ def update(dt: float) -> None:
 
 
 def draw() -> None:
-    image = generate_metaballs(Context.metaballs)
+    image = generate_metaballs(Context.metaballs, Context.palette)
     pr.update_texture(Context.texture, image.data)
+
     pr.draw_texture_pro(
         Context.texture,
         (0, 0, Context.texture.width, Context.texture.height),
@@ -41,3 +46,4 @@ def draw() -> None:
         0,
         pr.WHITE,
     )
+    pr.draw_fps(10, 10)
