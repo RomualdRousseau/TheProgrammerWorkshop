5635455546aa2d94b56a37afbfc2df5f23e7a2aa
Romuald Rousseau <romualdrousseau@gmail.com>
2025-05-15 10:33:08 +0200

    refacto: revice the physics model

--- TinyRpg/src/tinyrpg/sprites/sprite.py
+++ TinyRpg/src/tinyrpg/sprites/sprite.py
@@ -1,26 +1,31 @@
 import pyray as pr
 
-
+from tinyrpg import EPSILON
 from tinyrpg.sprites.animation import Animation
 
 
 class Sprite:
     def __init__(self, texture: pr.Texture, pos: pr.Vector2) -> None:
         self.texture = texture
+        self.mass = 1.0
+        self.force = pr.vector2_zero()
         self.pos = pos
         self.vel = pr.vector2_zero()
 
-    def move(self, force: pr.Vector2, mass: float, dt: float) -> None:
-        acc = pr.vector2_scale(force, 1 / mass)
-        self.vel = pr.vector2_add(self.vel, pr.vector2_scale(acc, dt))
-        self.pos = pr.vector2_add(self.pos, pr.vector2_scale(self.vel, dt))
+    def move(self, speed: pr.Vector2, dt: float) -> None:
+        mu = self.mass / (dt + EPSILON)
+        friction = pr.vector2_add(pr.vector2_scale(speed, mu - 1), pr.vector2_scale(self.vel, -mu))
+        self.force = pr.vector2_add(self.force, pr.vector2_add(speed, friction))
 
     def constrain_to_world(self, boundary: pr.Rectangle):
         self.pos.x = max(boundary.x, min(self.pos.x, boundary.x + boundary.width))
         self.pos.y = max(boundary.y, min(self.pos.y, boundary.y + boundary.height))
 
     def update(self, dt: float) -> None:
-        pass
+        acc = pr.vector2_scale(self.force, 1 / self.mass)
+        self.vel = pr.vector2_add(self.vel, pr.vector2_scale(acc, dt))
+        self.pos = pr.vector2_add(self.pos, pr.vector2_scale(self.vel, dt))
+        self.force = pr.vector2_zero()
 
     def draw(self) -> None:
         pr.draw_texture_v(self.texture, self.pos, pr.WHITE)
