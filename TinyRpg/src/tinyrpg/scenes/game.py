7e718daf332d9e5f3a23220b7607e2c28ffc4a1d
Romuald Rousseau <romualdrousseau@gmail.com>
2025-06-01 12:02:22 +0200

    feat: Add particles

--- TinyRpg/src/tinyrpg/scenes/game.py
+++ TinyRpg/src/tinyrpg/scenes/game.py
@@ -4,6 +4,8 @@ from functools import cache
 import pyray as pr
 
 from tinyrpg.engine.map import Map
+from tinyrpg.engine.particle import Particle
+from tinyrpg.particles.hey import Hey
 from tinyrpg.resources import load_map, load_music, unload_resources
 from tinyrpg.sprites.hero import Hero
 from tinyrpg.utils.draw_manager import begin_draw
@@ -15,6 +17,7 @@ class Game:
     map: Map
     music: pr.Music
     hero: Hero
+    particles: list[Particle]
 
 
 @cache
@@ -24,6 +27,7 @@ def get_game(level_name: str) -> Game:
         load_map(level_name + "_map"),
         load_music(level_name + "_music"),
         Hero(pr.Vector2(0, -24)),
+        [],
     )
 
 
@@ -41,9 +45,13 @@ def update(dt: float) -> None:
     game = get_game("level1")
     pr.update_music_stream(game.music)
     game.hero.update(dt)
+    for particle in game.particles:
+        particle.update(dt)
+    game.particles = [particle for particle in game.particles if particle.is_alive()]
 
     has_collision, collision_vector = game.map.check_collide_bbox(game.hero.get_bbox(), pr.vector2_zero())
     if has_collision:
+        game.particles.append(Hey(pr.Vector2(game.hero.pos.x + 16, game.hero.pos.y - 8)))
         game.hero.collide(collision_vector, dt)
 
 
@@ -54,4 +62,6 @@ def draw() -> None:
     with begin_draw(game.camera):
         game.map.draw()
         game.hero.draw()
+        for particle in game.particles:
+            particle.draw()
     pr.draw_fps(10, 10)
