8f71f0d6539e56647b86f817c016eccc62de6960
Romuald Rousseau <romuald.rousseau@servier.com>
2025-06-25 17:50:47 +0200

    fix: Fix shopping cart overflow

---
+++ TinyRpg/tests/tinyrpg/engine/utils/test_quad_tree.py
@@ -0,0 +1,67 @@
+from __future__ import annotations
+
+from functools import partial
+
+import pyray as pr
+import pytest
+
+from tinyrpg.engine.utils.bbox import check_collision_bbox_point
+from tinyrpg.engine.utils.quad_tree import QuadTreeBuilder
+
+
+@pytest.fixture
+def quad_tree():
+    builder = QuadTreeBuilder[float](size=4, spacing=1.0)
+    return builder.build()
+
+
+def test_build_tree(quad_tree):
+    assert len(quad_tree.children) == 4
+    for child in quad_tree.children:
+        assert len(child.children) == 4
+
+
+def test_walk_tree(quad_tree):
+    point = pr.Vector2(0.5, 0.5)
+    nodes = list(quad_tree.walk_tree(partial(check_collision_bbox_point, point=point)))
+    assert len(nodes) == 1
+    assert nodes[0].bbox.min == (-0.5, -0.5, -1)
+    assert nodes[0].bbox.max == (0.5, 0.5, 1)
+
+
+def test_find_point(quad_tree):
+    point = pr.Vector2(0.5, 0.5)
+    quad_tree.append(point, 1.0)
+    entities = quad_tree.find_point(point)
+    assert entities == [1.0]
+
+
+def test_find_ray(quad_tree):
+    ray = pr.Ray(pr.Vector3(0, 0, 0), pr.Vector3(1, 1, 0))
+    point = pr.Vector2(0.5, 0.5)
+    quad_tree.append(point, 1.0)
+    entities = quad_tree.find_ray(ray)
+    assert entities == [1.0]
+
+
+def test_find_bbox(quad_tree):
+    bbox = pr.BoundingBox(pr.Vector3(0, 0, 0), pr.Vector3(1, 1, 0))
+    point = pr.Vector2(0.5, 0.5)
+    quad_tree.append(point, 1.0)
+    entities = quad_tree.find_bbox(bbox)
+    assert entities == [1.0]
+
+
+def test_append(quad_tree):
+    point = pr.Vector2(0.5, 0.5)
+    node = quad_tree.append(point, 1.0)
+    assert node is not None
+    assert node.entities == [1.0]
+
+
+def test_remove(quad_tree):
+    point = pr.Vector2(0.5, 0.5)
+    quad_tree.append(point, 1.0)
+    node = quad_tree.remove(point, 1.0)
+    assert node is not None
+    assert node.entities == []
