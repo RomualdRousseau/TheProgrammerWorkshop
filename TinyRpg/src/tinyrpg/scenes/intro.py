0a0e6d4438144eced51cbb46f19e5a86c5f7392d
Romuald Rousseau <romualdrousseau@gmail.com>
2025-07-12 15:07:44 +0200

    feat: Better game over scenes

--- TinyRpg/src/tinyrpg/scenes/intro.py
+++ TinyRpg/src/tinyrpg/scenes/intro.py
@@ -2,9 +2,12 @@ from typing import Any, Optional
 
 import pyray as pr
 
-from tinyrpg.engine import FixedCamera, Scene, SceneEvent, load_texture, unload_resources
-from tinyrpg.engine.base.drawing import draw_text_outlined_v
+from tinyrpg.constants import WORLD_HEIGHT, WORLD_WIDTH
+from tinyrpg.engine import FixedCamera, Scene, SceneEvent, Timer, draw_text_outlined_v, load_texture, unload_resources
 
+INTRO_FONT_SIZE = 48
+
+timer = Timer(5)
 events: list[SceneEvent] = []
 
 
@@ -16,6 +19,7 @@ def init(previous_scene: Optional[Scene] = None):
     global texture, fixed_camera
     fixed_camera = FixedCamera()
     texture = load_texture("screen-intro")
+    timer.set()
 
 
 def release():
@@ -23,7 +27,9 @@ def release():
 
 
 def update(dt: float):
-    if pr.is_key_pressed(pr.KeyboardKey.KEY_X):
+    timer.update(dt)
+
+    if timer.is_elapsed() or pr.is_key_pressed(pr.KeyboardKey.KEY_X):
         events.append(SceneEvent("change", ("intro", "keypress")))
 
 
@@ -31,13 +37,9 @@ def draw():
     pr.begin_mode_2d(fixed_camera.camera)
     pr.draw_texture(texture, 0, 0, pr.WHITE)
 
-    text_width = pr.measure_text("TinyRpg", 48)
-    text_pos = pr.Vector2(128 - text_width / 2, 128 - 48 / 2)
-    draw_text_outlined_v("TinyRpg", text_pos, 48, pr.WHITE, pr.BLACK)
-
-    text_width = pr.measure_text("Press [X] key to play ...", 8)
-    text_pos = pr.Vector2(128 - text_width / 2, 128 + 48 / 2 + 8)
-    draw_text_outlined_v("Press [X] key to play ...", text_pos, 8, pr.WHITE, pr.BLACK)
+    text_width = pr.measure_text("TinyRpg", INTRO_FONT_SIZE)
+    text_pos = pr.Vector2((WORLD_WIDTH - text_width) / 2, (WORLD_HEIGHT - INTRO_FONT_SIZE) / 2)
+    draw_text_outlined_v("TinyRpg", text_pos, INTRO_FONT_SIZE, pr.WHITE, pr.BLACK)
 
     pr.end_mode_2d()
 
